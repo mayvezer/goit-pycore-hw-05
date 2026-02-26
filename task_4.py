3071caf1eb03899293f24c2ed8421f42134ac2ad
a-yesin <yesin2599sasha@gmail.com>
2026-02-26 21:30:22 +0200

    Initial commit with correct structure

---
+++ task_4.py
@@ -0,0 +1,80 @@
+def input_error(func):
+ 
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+
+        except ValueError:
+            return "Give me name and phone please."
+
+        except KeyError:
+            return "Contact not found."
+
+        except IndexError:
+            return "Enter user name."
+
+    return inner
+
+
+@input_error
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+
+@input_error
+def get_phone(args, contacts):
+    name = args[0]
+    return contacts[name]
+
+
+@input_error
+def get_all(contacts):
+    if not contacts:
+        return "No contacts saved."
+
+    result = ""
+    for name, phone in contacts.items():
+        result += f"{name}: {phone}\n"
+
+    return result.strip()
+
+
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    return cmd.lower(), args
+
+
+def main():
+    contacts = {}
+
+    print("Welcome to the assistant bot!")
+
+    while True:
+        user_input = input("Enter a command: ")
+
+        command, args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+
+        elif command == "hello":
+            print("How can I help you?")
+
+        elif command == "add":
+            print(add_contact(args, contacts))
+
+        elif command == "phone":
+            print(get_phone(args, contacts))
+
+        elif command == "all":
+            print(get_all(contacts))
+
+        else:
+            print("Invalid command.")
+
+
+if __name__ == "__main__":
+    main()
